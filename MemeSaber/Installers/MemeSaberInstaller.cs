f673aab188d57162136532da9e2cf3afc9694f2a
TeknozFR <teknoz.fr@gmail.com>
2021-02-11 21:13:17 -0500

    Add project files.

---
+++ MemeSaber/Installers/MemeSaberInstaller.cs
@@ -0,0 +1,18 @@
+﻿using MemeSaber.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace MemeSaber.Installers
+{
+    class MemeSaberInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<MemeViewController>().AsSingle();
+        }
+    }
+}
---
+++ MemeSaber/UI/MemeViewController.cs
@@ -0,0 +1,100 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using HMUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+using TMPro;
+using System.IO;
+using Random = System.Random;
+using System.Diagnostics;
+
+namespace MemeSaber.UI
+{
+    class MemeViewController : IInitializable, IDisposable
+    {
+        //string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+        DirectoryInfo directoryMemes = new DirectoryInfo(@"C:\Users\tekno\source\repos\MemeSaber\MemeSaber\Memes");
+        List<Sprite> memeList = new List<Sprite>();
+        List<int> usedMemes = new List<int>();
+        Random random = new Random();
+
+
+        private ResultsViewController resultsViewController;
+
+        [UIComponent("meme-image")]
+        private ImageView memeImage;
+
+        [UIComponent("root")]
+        private RectTransform rootTransform;
+
+        [UIComponent("all-meme-used-text")]
+        private TextMeshProUGUI modifiedText;
+
+        MemeViewController(ResultsViewController resultsViewController)
+        {
+            this.resultsViewController = resultsViewController;
+        }
+
+        public void Initialize()
+        {
+            BSMLParser.instance.Parse(BeatSaberMarkupLanguage.Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "MemeSaber.UI.MemeView.bsml"), resultsViewController.gameObject, this);
+            rootTransform.Translate(new Vector3(0, 0.80f, 0));
+            resultsViewController.didActivateEvent += ResultsViewController_didActivateEvent;
+
+            FileInfo[] Files = directoryMemes.GetFiles();
+            foreach (FileInfo imagePath in Files)
+            {
+                string imageFileName = imagePath.ToString().Replace(@"C:\Users\tekno\source\repos\MemeSaber\MemeSaber\Memes\", "");
+                Plugin.Log.Info("Loaded meme file: " + imageFileName);
+                memeList.Add(BeatSaberMarkupLanguage.Utilities.FindSpriteInAssembly(("MemeSaber.Memes." + imageFileName)));
+            }
+        }
+
+        private void ResultsViewController_didActivateEvent(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            modifiedText.text = "";
+            usedMemes.Clear();
+            OnButtonClick();
+        }
+
+        public void Dispose()
+        {
+            resultsViewController.didActivateEvent -= ResultsViewController_didActivateEvent;
+        }
+
+       
+        [UIAction("on-click-button")]
+        public void OnButtonClick()
+        {
+            while (true)
+            {
+                int randomNumber = random.Next(0, memeList.Count());
+
+
+
+                if (usedMemes.Contains(randomNumber) == false)
+                {
+                    memeImage.sprite = memeList[randomNumber];
+                    usedMemes.Add(randomNumber);
+
+
+                    break;
+                }
+
+                else if (usedMemes.Count() == memeList.Count())
+                {
+                    usedMemes.Clear();
+
+                    modifiedText.text = "No more new memes!";
+                }
+
+            }          
+        }
+    }
+}
